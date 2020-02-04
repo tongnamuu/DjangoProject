d7ec34d74199db01ef069dea417c82829ca92e02
tongnamuu <thban33@naver.com>
2020-02-05 04:49:48 +0900

    manual search filters Completed

--- rooms/views.py
+++ rooms/views.py
@@ -79,7 +79,38 @@ def search(request):
         "amenities": amenities,
         "facilities": facilities,
     }
-    return render(request, "rooms/search.html", {**form, **choices},)
+
+    filter_args = {}
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+    filter_args["country"] = country
+    if room_type != 0:
+        filter_args["room_type__pk__exact"] = room_type
+    if price != 0:
+        filter_args["price__lte"] = price
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+    if bool(instant) is True:
+        filter_args["instant_book"] = True
+    if bool(superhost) is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facillities__pk"] = int(s_facility)
+
+    rooms = models.Room.objects.filter(**filter_args)
+    print(filter_args)
+    print(rooms)
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms},)
 
 
 # def room_detail(request, pk):
