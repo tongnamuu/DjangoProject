2a84683c66f2ec5002f31c5db31718a689f777e6
tongnamuu <thban33@naver.com>
2020-02-05 04:08:26 +0900

    manual search Form completed

--- rooms/views.py
+++ rooms/views.py
@@ -10,6 +10,8 @@ from django.utils import timezone
 from django.urls import reverse
 from django.http import Http404
 
+from django_countries import countries
+
 # Create your views here.
 
 
@@ -36,6 +38,48 @@ class RoomDetail(DetailView):
 
     model = models.Room
     # pk_url_kwarg = "apple" url의 pk대신 apple 사용가능
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    instant = request.GET.get("instant", False)
+    superhost = request.GET.get("superhost", False)
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "superhost": superhost,
+    }
+    # print(s_amenities)
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+    return render(request, "rooms/search.html", {**form, **choices},)
 
 
 # def room_detail(request, pk):
