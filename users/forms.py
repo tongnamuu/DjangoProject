3546fe873ae850251277aa96b9cec1d87fad1716
tongnamuu <thban33@naver.com>
2020-02-08 23:29:33 +0900

    Validating Email and Password Completed

--- users/forms.py
+++ users/forms.py
@@ -7,15 +7,23 @@ class LoginForm(forms.Form):
     email = forms.EmailField(required=True)
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     try:
+    #         models.User.objects.get(username=email)
+    #         return email
+    #     except models.User.DoesNotExist:
+    #         raise forms.ValidationError("User Does Not Exist")
+
+    def clean(self):
         email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User Does Not Exist")
-
-    def clean_password(self):
         password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is Wrong!"))
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User Does Not Exist"))
 
-        return password
