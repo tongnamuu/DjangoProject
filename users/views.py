cb84ebfd2381f40e8074575b1265436c2cdba7f6
tongnamuu <thban33@naver.com>
2020-02-10 16:55:14 +0900

    Manual Login Logout Completed

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 # Create your views here.
@@ -21,6 +22,20 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                print("######login#########")
+                return redirect(reverse("core:home"))
+            else:
+                print("Error")
         return render(request, "users/login.html", {"form": form,})
 
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
